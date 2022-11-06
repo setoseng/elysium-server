b0ed803975a6fb008d83df5b171593af03e81700
Seto <setoseng@gmail.com>
2022-11-06 13:27:18 -0600

    Implemented Routes for server

--- routes/users.js
+++ routes/users.js
@@ -10,9 +10,21 @@ router.get('/new' , (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  res.send("Create User")
+  res.send("Create User");
 })
 
+router
+  .route("/id:")
+  .get((req, res) => {
+    res.send(`Get User By User ID ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Update User by User ID ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete User by User ID ${req.params.id}`);
+  })
+  
 router.get('/:id', (req, res) => {
   res.send(`Get User With ID ${req.params.id}`);
 })
